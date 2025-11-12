a0601cc67fa84a1661317ad1dbaab1e6552f948a
MPaolaP <162258272+MPaolaP@users.noreply.github.com>
2025-11-11 22:12:01 -0500

    startup

--- startup.sh
+++ startup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-gunicorn --bind=0.0.0.0:8000 --timeout=600 app:app
+
+# Instalar dependencias del sistema para pyodbc
+apt-get update
+apt-get install -y unixodbc-dev unixodbc g++
+
+# Instalar las dependencias de Python
+pip install -r requirements.txt
+
+# Iniciar la aplicación
+gunicorn --bind=0.0.0.0 --timeout 600 app:app
